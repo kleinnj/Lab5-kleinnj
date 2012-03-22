94bdc98e8d2784f1b5ef5dc6ced99ebc8c2a2766
Nick Klein <kleinnj@rose-hulman.edu>
2012-03-22 15:10:59 -0400

    first test case made

---
+++ Lab5SQA/src/TestClass.java
@@ -0,0 +1,15 @@
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+
+
+public class TestClass {
+	
+	@Test
+	public void Test1ReturnsEmptyString(){
+		assertEquals("", PrimeFactors(1));
+	}
+	
+}
